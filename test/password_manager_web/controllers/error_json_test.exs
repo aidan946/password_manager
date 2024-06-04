153940d0f4d551f97cf7abf1bb629d72b9001cf8
Aidan Stevens <aidan@lookedafter.com>
2024-06-04 23:19:17 +1000

    Add extra error pages

--- test/password_manager_web/controllers/error_json_test.exs
+++ test/password_manager_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule PasswordManagerWeb.ErrorJSONTest do
   use PasswordManagerWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert PasswordManagerWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert PasswordManagerWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
